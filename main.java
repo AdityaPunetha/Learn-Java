49d0ff0d83b58b23af420d9275fb30cf6501cff5
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2021-08-15 09:40:59 +0530

    till operators

---
+++ main.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args) {
+        // primitive
+        int hello_world = 5;
+        double num2 = 5.0;
+        boolean b = true;
+        char c = 't';
+        String str = "wdjhwqdkhw";
+        int tim = hello_world;
+        //
+        int x;
+        x = 6;
+
+        System.out.println(tim);
+    }
+}
