6be33c53a4dad4efe7471f667ad001c0d374ac77
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2021-08-31 19:44:10 +0530

    162

--- main.java
+++ main.java
@@ -3,33 +3,66 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // primitive
+        // -----------------------------------------------------------------------------
         int hello_world = 5;
         double num2 = 5.0;
-        boolean b = true;
+        boolean b1 = true;
         char c = 't';
         String str = "wdjhwqdkhw";
         int tim = hello_world;
-        //
+        // -----------------------------------------------------------------------------
         int x;
         x = 6;
-        // int y = 5;
+        int y = 5;
         // double d = Math.pow(x, y);
         double u = x / (double) y;
         System.out.println(u);
-        // input
+        // input-----------------------------------------------------------------------------
         Scanner sc = new Scanner(System.in);
         String scanned = sc.next();
         System.out.println(scanned);
-        // integer input
+        // integer
+        // input-----------------------------------------------------------------------------
         Scanner sc2 = new Scanner(System.in);
         int scanned2 = sc2.nextInt();
-        // boolean
+        // boolean-----------------------------------------------------------------------------
         Scanner sc3 = new Scanner(System.in);
         boolean scanned3 = sc3.nextBoolean();
-        // similarly double, what we should do (typecasting)
+        // similarly double, what we should do
+        // (typecasting)------------------------------------
         Scanner sc4 = new Scanner(System.in);
         String scanned4 = sc4.next();
         int k = Integer.parseInt(scanned4);
-
+        // conditions and booleans-------------------------------------------------
+        x = 6;
+        y = 7;
+        int z = 10;
+        boolean compare = x < y;
+        System.out.println(compare);
+        // compare
+        // strings-----------------------------------------------------------------------------
+        String a = "hello";
+        String b = "h1";
+        System.out.println(a.equals(b));
+        compare = a == b;
+        // and-----------------------------------------------------------------------------
+        y = 23;
+        compare = x > y && z < y;
+        System.out.println(compare);
+        // or-----------------------------------------------------------------------------
+        compare = false || true;
+        System.out.println(compare);
+        compare = !(false || true);
+        System.out.println(compare);
+        // if-else-----------------------------------------------------------------------------
+        a = "hello";
+        b = "tim";
+        if (a.equals(b)) {
+            System.out.println("type tim");
+        } else if (a.equals("hello")) {
+            System.out.println("hi!");
+        } else {
+            System.out.println("print");
+        }
     }
 }
